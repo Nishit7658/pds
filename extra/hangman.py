1753f8faa1704e17a42c547c3d17060059b780ec
nishit panchal <nishit8758@gmail.com>
2026-09-05 21:48:57 +0530

    hangman

--- extra/hangman.py
+++ extra/hangman.py
@@ -15,7 +15,7 @@ print("""
 word_list = [
     "apple", "banana", "orange", "grape", "mango",
     "table", "chair", "window", "door", "house",
-    "school", "college", "teacher", "student", "book",
+    "school", "college", "teacher", "student", "book", 
     "pencil", "notebook", "computer", "keyboard", "mouse",
     "phone", "camera", "bottle", "glass", "plate",
     "river", "mountain", "forest", "ocean", "desert",
@@ -37,77 +37,233 @@ word_list = [
 def get_random_word(word_bank):
     return random.choice(word_bank)
 
-chosen_word = get_random_word(word_list)       
-print(chosen_word)
+
+chosen_word = get_random_word(word_list)
 
 print()
+
 print()
 
-
-count = 1
 life = 6
 
 print("Word to guess:: ")
+
 print()
 
 for i in range(len(chosen_word)):
-    print("_", end = " ")
+    print("_", end=" ")
 
 word_guess = []
 
 for i in range(len(chosen_word)):
     word_guess.append("_")
 
+guessed_letters = []
+
+hangman = [
+    """
+     -----
+     |   |
+         |
+         |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+         |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+     |   |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|   |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\  |
+    /    |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\  |
+    / \  |
+         |
+    =========
+    """
+]
+
 while life > 0:
 
-    display = ""
     print()
-    guess = input("Guess a letter :: ").lower()
 
-    for letter in range(len(chosen_word)):
-        if chosen_word[letter] == guess:
-            word_guess[letter] = guess
-            display += chosen_word[letter]
-        
-            # print(letter)
-            # for guess in chosen_word:
-            #     if letter == guess:
-            #         print(letter)
-            #     else:
-            #         print("_")
-        else:
-            display += "_"
-    
     print(" ".join(word_guess))
 
-#     Start
+    print()
 
-# Lives = 6
+    print("Lives left ::", life)
 
-# While lives > 0
+    print()
 
-#     1. Show the current word
-#        Example:
-#        Word to guess: a_____
+    guess = input("Guess a letter :: ").lower()
 
-#     2. Ask the user for ONE letter
-#        Guess a letter:
+    if len(guess) != 1:
+        print("Please enter ONE letter.")
+        continue
 
-#     3. Check if the letter is in the hidden word.
+    if not guess.isalpha():
+        print("Please enter a letter.")
+        continue
 
-#         If YES
-#             Reveal all matching letters.
-#             Example:
-#             a__l_
+    if guess in guessed_letters:
+        print("You already guessed this letter.")
+        continue
 
-#             If the whole word is revealed
-#                 You Win
-#                 Stop
+    guessed_letters.append(guess)
 
-#         If NO
-#             Lose one life.
-#             Show the next Hangman picture.
+    if guess in chosen_word:
 
-#     4. Show how many lives are left.
+        for letter in range(len(chosen_word)):
 
-# Repeat
+            if chosen_word[letter] == guess:
+                word_guess[letter] = guess
+
+        print("Correct Guess !!")
+
+    else:
+
+        life -= 1
+
+        print("Wrong Guess !!")
+
+        print(hangman[6 - life])
+
+    if "_" not in word_guess:
+
+        print()
+
+        print(" ".join(word_guess))
+
+        print()
+
+        print("You Win !!")
+
+        print("The word was ::", chosen_word)
+
+        break
+
+else:
+
+    print()
+
+    print(hangman[5])
+
+    print()
+
+    print("You Lose !!")
+
+    print("The word was ::", chosen_word)
+
+
+
+# def get_random_word(word_bank):
+#     return random.choice(word_bank)
+
+# chosen_word = get_random_word(word_list)       
+# print(chosen_word)
+
+# print()
+# print()
+
+
+# count = 1
+# life = 6
+
+# print("Word to guess:: ")
+# print()
+
+# for i in range(len(chosen_word)):
+#     print("_", end = " ")
+
+# word_guess = []
+
+# for i in range(len(chosen_word)):
+#     word_guess.append("_")
+
+# while life > 0:
+
+#     display = ""
+#     print()
+#     guess = input("Guess a letter :: ").lower()
+
+#     for letter in range(len(chosen_word)):
+#         if chosen_word[letter] == guess:
+#             word_guess[letter] = guess
+#             display += chosen_word[letter]
+        
+#             # print(letter)
+#             # for guess in chosen_word:
+#             #     if letter == guess:
+#             #         print(letter)
+#             #     else:
+#             #         print("_")
+#         else:
+#             display += "_"
+    
+#     print(" ".join(word_guess))
+
+# #     Start
+
+# # Lives = 6
+
+# # While lives > 0
+
+# #     1. Show the current word
+# #        Example:
+# #        Word to guess: a_____
+
+# #     2. Ask the user for ONE letter
+# #        Guess a letter:
+
+# #     3. Check if the letter is in the hidden word.
+
+# #         If YES
+# #             Reveal all matching letters.
+# #             Example:
+# #             a__l_
+
+# #             If the whole word is revealed
+# #                 You Win
+# #                 Stop
+
+# #         If NO
+# #             Lose one life.
+# #             Show the next Hangman picture.
+
+# #     4. Show how many lives are left.
+
+# # Repeat
